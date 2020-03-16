99f9a3f49163d9aac4326a0eb01de58449f8393e
Luis de la peña <luis.delapena@hotmail.es>
2020-03-16 11:15:07 -0400

    testing for template_methods and specific checkout

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -7,3 +7,36 @@ libraries{
 	}
 	kubernetes
 }
+
+
+
+allow_scm_jenkinsfile = false
+// skip the default JTE checkout and do it explicitly
+skip_default_checkout = true
+// define application environment objects
+application_environments{
+    dev{
+        long_name = "Development"
+    }
+    prod{
+        long_name = "Production"
+    }
+}
+
+/*
+  define libraries to load.
+  available libraries are based upon the
+  library sources configured.
+
+libraries{
+    github
+    sonarqube
+    docker
+    // library configurations are determined by
+    // the specific library implementation
+    openshift{
+        url = "https://example.openshift.com"
+        cred_id = "openshift"
+    }
+}
+*/
